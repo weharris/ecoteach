2299caeaed9ec0ca6cdffbcbaa27c5b766b0fe58
W. Edwin Harris <weh9000@gmail.com>
2025-06-24 13:19:15 +0100

    Enhance barnswallow_brightness dataset documentation with detailed descriptions, updated source citation, and meaningful teaching examples for improved educational context

--- R/barnswallow_brightness.R
+++ R/barnswallow_brightness.R
@@ -1,15 +1,24 @@
-
-#' Data from: North
+#' Barn Swallow Plumage Brightness and Mate Selection
 #'
-#' [ADD DETAILED DESCRIPTION HERE]
+#' This dataset contains information on plumage brightness measurements for North American 
+#' barn swallows (Hirundo rustica erythrogaster) and their mating patterns. The data includes 
+#' measurements of belly and breast brightness for social pairs and extra-pair mates.
+#' This dataset was used to study assortative mating patterns in barn swallows, investigating
+#' how plumage coloration affects mate selection both within social pairs and through
+#' extra-pair fertilizations.
 #'
-#' @format A data frame with [X] rows and [Y] variables:
+#' @format A data frame with 19 rows and 7 variables:
 #' \describe{
-#'   \item{column1}{Description of column 1}
-#'   \item{column2}{Description of column 2}
-#'   [ADD ALL COLUMNS HERE]
+#'   \item{MaleID}{Band ID for the focal male}
+#'   \item{PairID}{Band ID for the female social mate}
+#'   \item{EPID}{Band ID for the extra-pair mate}
+#'   \item{PairB_bright}{Belly brightness for the social male (unitless measurement of reflectance)}
+#'   \item{PairR_bright}{Breast brightness for the social male (unitless measurement of reflectance)}
+#'   \item{EPB_bright}{Belly brightness for the extra-pair male (unitless measurement of reflectance)}
+#'   \item{EPR_bright}{Breast brightness for the extra-pair male (unitless measurement of reflectance)}
 #' }
-#' @source Morosse, Omar and Tsunekage, Toshi and Kenny-Duddela, Heather and Schield, Drew and Keller, Kayleigh and Safran, Rebecca and Levin, Iris (2025). Data from: North. Dryad Digital Repository. 
+#' @source Morosse, Omar, Tsunekage, Toshi, Kenny-Duddela, Heather, Schield, Drew, Keller, Kayleigh, Safran, Rebecca, & Levin, Iris (2025). 
+#'   North American barn swallows pair, mate, and interact assortatively. Dryad Digital Repository. 
 #'   \doi{10.5061/DRYAD.1G1JWSV8G}
 #' @examples
 #' \donttest{
@@ -20,7 +29,18 @@
 #' head(barnswallow_brightness)
 #' summary(barnswallow_brightness)
 #' 
-#' # Add relevant examples here
+#' # Compare brightness between social and extra-pair males
+#' boxplot(barnswallow_brightness$PairB_bright, barnswallow_brightness$EPB_bright,
+#'         names = c("Social Male", "Extra-pair Male"),
+#'         main = "Comparison of Belly Brightness",
+#'         ylab = "Brightness")
+#'         
+#' # Correlation between social and extra-pair male brightness
+#' plot(barnswallow_brightness$PairB_bright, barnswallow_brightness$EPB_bright,
+#'      main = "Correlation between Social and Extra-pair Male Brightness",
+#'      xlab = "Social Male Belly Brightness",
+#'      ylab = "Extra-pair Male Belly Brightness")
+#' abline(lm(EPB_bright ~ PairB_bright, data = barnswallow_brightness), col = "red")
 #' }
 "barnswallow_brightness"
 
